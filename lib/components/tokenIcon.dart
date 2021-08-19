a2b46287d9bec5b182654071251624a2e2226145
shawn <dhryxy@gmail.com>
2021-08-19 16:56:39 +0800

    replace TokenIcon param symbol with id

--- lib/components/tokenIcon.dart
+++ lib/components/tokenIcon.dart
@@ -2,14 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TokenIcon extends StatelessWidget {
-  TokenIcon(this.symbol, this.tokenIcons, {this.small = false});
+  TokenIcon(this.id, this.tokenIcons, {this.small = false, this.symbol});
+  final String id;
   final String symbol;
   final Map<String, Widget> tokenIcons;
   final bool small;
   @override
   Widget build(BuildContext context) {
-    if (symbol.contains('-')) {
-      final pair = symbol.toUpperCase().split('-');
+    if (id.contains('-')) {
+      final pair = id.toUpperCase().split('-');
       return SizedBox(
         child: Stack(
           children: [
@@ -17,20 +18,15 @@ class TokenIcon extends StatelessWidget {
               margin: EdgeInsets.only(left: 16),
               child: tokenIcons[pair[1]],
             ),
-            SizedBox(
-              width: 29,
-              child: tokenIcons[pair[0]],
-            )
+            SizedBox(width: 29, child: tokenIcons[pair[0]])
           ],
         ),
         width: 45,
       );
     }
     return SizedBox(
-        child: tokenIcons[symbol.toUpperCase()] ??
-            CircleAvatar(
-              child: Text(symbol.substring(0, 2)),
-            ),
+        child: tokenIcons[id.toUpperCase()] ??
+            CircleAvatar(child: Text((symbol ?? id).substring(0, 2))),
         width: small ? 24 : 32);
   }
 }
