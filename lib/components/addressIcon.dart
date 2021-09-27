173734d3158d0879e69b4ab739ca8766877df937
丁祖华 <dingzuhua@dingzuhuadeMacBook-Pro.local>
2021-09-27 11:21:34 +0800

    add inputItem

--- lib/components/addressIcon.dart
+++ lib/components/addressIcon.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:polkawallet_ui/utils/index.dart';
 
 class AddressIcon extends StatelessWidget {
-  AddressIcon(
-    this.address, {
-    this.size,
-    this.svg,
-    this.tapToCopy = true,
-  });
+  AddressIcon(this.address,
+      {this.size,
+      this.svg,
+      this.tapToCopy = true,
+      this.borderColor,
+      this.borderWidth = 0.5});
   final String? address;
   final String? svg;
   final double? size;
   final bool tapToCopy;
+  final Color? borderColor;
+  final double borderWidth;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +24,13 @@ class AddressIcon extends StatelessWidget {
       child: Container(
         width: size ?? 40,
         height: size ?? 40,
+        decoration: borderColor == null
+            ? null
+            : BoxDecoration(
+                borderRadius:
+                    BorderRadius.all(Radius.circular((size ?? 40) / 2.0)),
+                border: Border.all(color: borderColor!, width: borderWidth),
+              ),
         child: svg == null
             ? Image.asset(
                 'packages/polkawallet_ui/assets/images/polkadot_avatar.png',
---
+++ lib/components/inputItem.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class InputItem extends StatefulWidget {
+  Color? color;
+  final double borderWidth;
+  EdgeInsetsGeometry? padding;
+  TextAlign textAlign;
+  TextInputType textInputType;
+  Function(String) onChanged;
+  String hintText;
+  TextStyle style;
+  TextStyle hintStyle;
+  TextEditingController? controller;
+  InputItem(
+      {required this.onChanged,
+      required this.hintText,
+      required this.style,
+      required this.hintStyle,
+      this.color,
+      this.borderWidth = 0.5,
+      this.padding,
+      this.textAlign = TextAlign.start,
+      this.textInputType = TextInputType.text,
+      this.controller,
+      Key? key})
+      : super(key: key);
+
+  @override
+  _InputItemState createState() => _InputItemState();
+}
+
+class _InputItemState extends State<InputItem> {
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      widget.controller = widget.controller ?? TextEditingController();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+        border: Border.all(
+            color: widget.color ?? Theme.of(context).disabledColor,
+            width: widget.borderWidth),
+      ),
+      padding: widget.padding ?? EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+              child: TextField(
+                  controller: widget.controller,
+                  textAlign: widget.textAlign,
+                  keyboardType: widget.textInputType,
+                  onChanged: widget.onChanged,
+                  style: widget.style,
+                  decoration: InputDecoration(
+                    isDense: true,
+                    hintText: widget.hintText,
+                    border: InputBorder.none,
+                    hintStyle: widget.hintStyle,
+                  ))),
+          GestureDetector(
+            child: Container(
+              padding: EdgeInsets.all(2),
+              child: Icon(
+                Icons.cancel,
+                size: 20,
+                color: widget.color ?? Theme.of(context).unselectedWidgetColor,
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                widget.controller!.text = "";
+              });
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
