66dae1042a32aa343c8587484f608da211999337
dingzuhua <dingzuhua@qq.com>
2021-12-22 17:53:42 +0800

    v3 ui add SkaletionRow

--- lib/components/SkaletonList.dart
+++ lib/components/SkaletonList.dart
@@ -1,6 +1,81 @@
 import 'package:flutter/material.dart';
-import 'package:polkawallet_ui/components/roundedCard.dart';
+import 'package:polkawallet_ui/components/v3/roundedCard.dart';
 import 'package:skeleton_loader/skeleton_loader.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SkaletionRow extends StatelessWidget {
+  SkaletionRow(
+      {Key? key,
+      this.child,
+      this.items = 1,
+      this.period = const Duration(seconds: 2),
+      this.highlightColor = const Color(0xFFC0C0C0),
+      this.baseColor = const Color(0xFFE0E0E0),
+      this.direction = SkeletonDirection.ltr,
+      this.itemMargin,
+      this.itemPadding})
+      : super(key: key);
+  Widget? child;
+  final int items;
+  final Duration period;
+  final Color highlightColor;
+  final SkeletonDirection direction;
+  final Color baseColor;
+  final EdgeInsetsGeometry? itemMargin;
+  final EdgeInsetsGeometry? itemPadding;
+
+  @override
+  Widget build(BuildContext context) {
+    final list = [];
+    for (int i = 0; i < items; i++) {
+      list.add(i);
+    }
+    return Row(
+      children: [
+        ...list
+            .map((e) => Expanded(
+                child: RoundedCard(
+                    margin: itemMargin ?? EdgeInsets.symmetric(horizontal: 3.w),
+                    padding: itemPadding ??
+                        EdgeInsets.symmetric(horizontal: 7.w, vertical: 3.h),
+                    child: SkeletonLoader(
+                      builder: child ??
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    width: 10,
+                                    height: 10,
+                                    margin: EdgeInsets.only(right: 3),
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10 / 2))),
+                                  ),
+                                  Container(
+                                      width: 30,
+                                      height: 17,
+                                      color: Colors.white),
+                                ],
+                              ),
+                              SizedBox(height: 3.5),
+                              Container(
+                                  width: 50, height: 15, color: Colors.white),
+                            ],
+                          ),
+                      items: 1,
+                      period: period,
+                      highlightColor: highlightColor,
+                      baseColor: baseColor,
+                      direction: direction,
+                    ))))
+            .toList()
+      ],
+    );
+  }
+}
 
 class SkaletonList extends StatelessWidget {
   SkaletonList(
@@ -11,7 +86,8 @@ class SkaletonList extends StatelessWidget {
       this.highlightColor = const Color(0xFFC0C0C0),
       this.baseColor = const Color(0xFFE0E0E0),
       this.direction = SkeletonDirection.ltr,
-      this.showDecoration = true})
+      this.itemMargin,
+      this.itemPadding})
       : super(key: key);
 
   Widget? child;
@@ -20,7 +96,8 @@ class SkaletonList extends StatelessWidget {
   final Color highlightColor;
   final SkeletonDirection direction;
   final Color baseColor;
-  final bool showDecoration;
+  final EdgeInsetsGeometry? itemMargin;
+  final EdgeInsetsGeometry? itemPadding;
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +105,10 @@ class SkaletonList extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
         itemCount: items,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-              margin: EdgeInsets.only(bottom: 16),
-              child: RoundedCard(
-                  child: SkeletonLoader(
+          return RoundedCard(
+              margin: itemMargin ?? EdgeInsets.only(bottom: 16),
+              padding: itemPadding,
+              child: SkeletonLoader(
                 builder: child ??
                     Container(
                       padding:
@@ -67,64 +144,7 @@ class SkaletonList extends StatelessWidget {
                 highlightColor: highlightColor,
                 baseColor: baseColor,
                 direction: direction,
-              )));
+              ));
         });
-    // return Container(
-    //     margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-    //     decoration: !showDecoration
-    //         ? null
-    //         : BoxDecoration(
-    //             borderRadius: const BorderRadius.all(const Radius.circular(16)),
-    //             color: Theme.of(context).cardColor,
-    //             boxShadow: [
-    //               BoxShadow(
-    //                 color: Colors.black12,
-    //                 blurRadius: 16.0, // has the effect of softening the shadow
-    //                 spreadRadius: 4.0, // has the effect of extending the shadow
-    //                 offset: Offset(
-    //                   2.0, // horizontal, move right 10
-    //                   2.0, // vertical, move down 10
-    //                 ),
-    //               )
-    //             ],
-    //           ),
-    //     child: SkeletonLoader(
-    //       builder: child ??
-    //           Container(
-    //             // margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-    //             padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-
-    //             child: Row(
-    //               children: <Widget>[
-    //                 CircleAvatar(
-    //                   radius: 30,
-    //                 ),
-    //                 SizedBox(width: 10),
-    //                 Expanded(
-    //                   child: Column(
-    //                     children: <Widget>[
-    //                       Container(
-    //                         width: double.infinity,
-    //                         height: 10,
-    //                         color: Colors.white,
-    //                       ),
-    //                       SizedBox(height: 10),
-    //                       Container(
-    //                         width: double.infinity,
-    //                         height: 12,
-    //                         color: Colors.white,
-    //                       ),
-    //                     ],
-    //                   ),
-    //                 ),
-    //               ],
-    //             ),
-    //           ),
-    //       items: items,
-    //       period: period,
-    //       highlightColor: highlightColor,
-    //       baseColor: baseColor,
-    //       direction: direction,
-    //     ));
   }
 }
