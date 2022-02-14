a4824f0e8e3a53907750f7640c64fb134b63b17e
dingzuhua <dingzuhua@qq.com>
2022-02-14 18:13:44 +0800

    update ui

--- lib/components/v3/plugin/pluginPageTitleTaps.dart
+++ lib/components/v3/plugin/pluginPageTitleTaps.dart
@@ -2,22 +2,27 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PluginPageTitleTaps extends StatelessWidget {
-  PluginPageTitleTaps({this.names, this.activeTab, this.onTap});
+  PluginPageTitleTaps(
+      {this.names, this.activeTab, this.onTap, this.isSpaceBetween = false});
 
   final List<String>? names;
   final Function(int)? onTap;
   final int? activeTab;
+  final bool isSpaceBetween;
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isSpaceBetween
+          ? MainAxisAlignment.spaceBetween
+          : MainAxisAlignment.start,
       children: names!.map(
         (title) {
           int index = names!.indexOf(title);
           return GestureDetector(
             child: Container(
               padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
-              margin: EdgeInsets.only(right: 26),
+              margin: EdgeInsets.only(right: isSpaceBetween ? 0 : 26),
               decoration: BoxDecoration(
                 borderRadius: const BorderRadius.all(const Radius.circular(6)),
                 color:
