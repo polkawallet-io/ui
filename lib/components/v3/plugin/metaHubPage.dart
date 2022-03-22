959be71e8590bf29eb38aaf52fdf073dcdf0bbf7
dingzuhua <dingzuhua@qq.com>
2022-03-22 16:28:11 +0800

    flutter_swiper update to card_swiper

--- lib/components/v3/plugin/metaHubPage.dart
+++ lib/components/v3/plugin/metaHubPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:card_swiper/card_swiper.dart';
 import 'package:polkawallet_ui/components/v3/plugin/tabBarPlugin.dart';
 
 class MetaHubPage extends StatefulWidget {
