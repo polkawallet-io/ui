fd6bfdb2e2c172e0308930982acf613c5b33fd47
dingzuhua <dingzuhua@qq.com>
2022-03-29 17:03:20 +0800

    update mainTabBar

--- lib/components/v3/mainTabBar.dart
+++ lib/components/v3/mainTabBar.dart
@@ -1,49 +1,63 @@
 import 'package:flutter/material.dart';
 
 class MainTabBar extends StatelessWidget {
-  MainTabBar({
-    this.tabs = const [],
-    this.activeTab,
-    this.onTap,
-  });
+  MainTabBar({required this.tabs, this.activeTab, this.onTap});
 
-  final List<String> tabs;
+  final Map<String, bool> tabs;
   final Function(int)? onTap;
   final int? activeTab;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: tabs.map((e) {
-        final index = tabs.indexOf(e);
-        return Expanded(
-          child: GestureDetector(
-            child: Container(
-              alignment: Alignment.center,
-              height: 36,
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                image: DecorationImage(
-                    image: AssetImage(
-                        "packages/polkawallet_ui/assets/images/tab_bg_${index == 0 ? 'l' : 'r'}_${index == activeTab ? 'blue' : 'grey'}.png"),
-                    fit: BoxFit.fill),
-              ),
-              child: Text(
-                e,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  fontFamily: 'TitilliumWeb',
-                  color: activeTab == index
-                      ? Colors.white
-                      : Theme.of(context).unselectedWidgetColor,
-                ),
-              ),
+    List<Widget> children = [];
+    tabs.forEach((key, value) {
+      final index = tabs.keys.toList().indexOf(key);
+      children.add(Expanded(
+        child: GestureDetector(
+          child: Container(
+            alignment: Alignment.center,
+            height: 36,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              image: DecorationImage(
+                  image: AssetImage(
+                      "packages/polkawallet_ui/assets/images/tab_bg_${index == 0 ? 'l' : 'r'}_${index == activeTab ? 'blue' : 'grey'}.png"),
+                  fit: BoxFit.fill),
             ),
-            onTap: () => onTap!(index),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  key,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: 'TitilliumWeb',
+                    color: activeTab == index
+                        ? Colors.white
+                        : Theme.of(context).unselectedWidgetColor,
+                  ),
+                ),
+                Visibility(
+                    visible: value,
+                    child: Container(
+                      width: 9,
+                      height: 9,
+                      margin: EdgeInsets.only(left: 5),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4.5),
+                          color: Theme.of(context).errorColor),
+                    ))
+              ],
+            ),
           ),
-        );
-      }).toList(),
+          onTap: () => onTap!(index),
+        ),
+      ));
+    });
+    return Row(
+      children: children,
     );
   }
 }
