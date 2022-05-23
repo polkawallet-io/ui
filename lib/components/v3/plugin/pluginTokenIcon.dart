ffd2e56e2620c004b0a93eb3b38577c18ba286cf
dingzuhua <dingzuhua@qq.com>
2022-05-23 17:39:01 +0800

    fix token icon bug

--- lib/components/v3/plugin/pluginTokenIcon.dart
+++ lib/components/v3/plugin/pluginTokenIcon.dart
@@ -50,35 +50,37 @@ class PluginTokenIcon extends StatelessWidget {
     }
 
     return SizedBox(
-        child: Stack(
-          children: [
-            Container(
-                decoration: ShapeDecoration(
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadiusDirectional.circular(size)),
+      width: size,
+      height: size,
+      child: Stack(
+        children: [
+          Container(
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusDirectional.circular(size)),
+              ),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(size),
+                  child: tokenIcons[id.toUpperCase()] ??
+                      CircleAvatar(
+                          child: Text((symbol ?? id).substring(
+                              0,
+                              (symbol ?? id).length > 2
+                                  ? 2
+                                  : (symbol ?? id).length))))),
+          Visibility(
+              visible: !isHighlighted,
+              child: Container(
+                width: size,
+                height: size,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(size / 2)),
+                  color: Color(0x33000000),
                 ),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(size),
-                    child: tokenIcons[id.toUpperCase()] ??
-                        CircleAvatar(
-                            child: Text((symbol ?? id).substring(
-                                0,
-                                (symbol ?? id).length > 2
-                                    ? 2
-                                    : (symbol ?? id).length))))),
-            Visibility(
-                visible: !isHighlighted,
-                child: Container(
-                  width: size,
-                  height: size,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(size / 2)),
-                    color: Color(0x33000000),
-                  ),
-                ))
-          ],
-        ),
-        width: size);
+              ))
+        ],
+      ),
+    );
   }
 }
