901e5b3ffbf5f509b5c2a0de965c89281d342bd4
dingzuhua <dingzuhua@qq.com>
2022-06-28 16:27:27 +0800

    fix ui bug

--- lib/pages/v3/xcmTxConfirmPage.dart
+++ lib/pages/v3/xcmTxConfirmPage.dart
@@ -751,12 +751,16 @@ class _XcmTxConfirmPageState extends State<XcmTxConfirmPage> {
                                         Visibility(
                                             visible: isNetworkConnected,
                                             child: Container(
-                                                height: 44,
+                                                height: 24,
                                                 width: 24,
-                                                margin:
-                                                    EdgeInsets.only(right: 8),
-                                                child: args.chainFromIcon ??
-                                                    Container())),
+                                                margin: EdgeInsets.only(
+                                                    right: 8,
+                                                    top: 10,
+                                                    bottom: 10),
+                                                child: Center(
+                                                  child: args.chainFromIcon ??
+                                                      Container(),
+                                                ))),
                                         !isNetworkConnected
                                             ? Text(
                                                 dic['tx.network.no']!,
